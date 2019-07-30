8c94f7d81471bd65c00e4921a94978a10e9413bf
EmptyInside <vladkovalsky1@gmail.com>
2019-07-30 11:51:30 +0200

    added html and css files + Python programming

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post1',
+        'content': 'First Post Content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
